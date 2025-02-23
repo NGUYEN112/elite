ab2a40aee0d485cf5cc5b7f60205670da4845a03
NguyenPNH <phannguyenhoainguyen@gmail.com>
2025-02-23 12:40:57 +0700

    finish coding home page

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,3 +4,13 @@ $(".home-slick").slick({
     appendDots: $(".custome-dots"),
     slideToShow: 1
 })
+
+window.addEventListener("scroll", fixedHeader);
+
+function fixedHeader() {
+  if(window.scrollY > 0) {
+    document.querySelector("nav.header__navBar").classList.add("fixed_bg");
+  }else {
+    document.querySelector("nav.header__navBar").classList.remove("fixed_bg");
+  }
+}
